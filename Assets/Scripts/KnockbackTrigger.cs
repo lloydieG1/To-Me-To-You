82a1454f7d1cd8797d687ad22d33409bc00ff713
IestynGreen <20207508@hope.ac.uk>
2023-03-12 17:19:42 +0000

    some stuff

--- Assets/Scripts/KnockbackTrigger.cs
+++ Assets/Scripts/KnockbackTrigger.cs
@@ -8,8 +8,13 @@ public class KnockbackTrigger : MonoBehaviour
 
 private void OnCollisionEnter2D(Collision2D other) {
     var PlayerMovement = other.collider.GetComponent<PlayerMovement>();
-    if(PlayerMovement != null){
+    if(PlayerMovement != null && transform.tag == "Enemy"){
         PlayerMovement.Knockback(transform);
     }
+
+    // var WalkToTarget = other.collider.GetComponent<WalkToTarget>();
+    // if(WalkToTarget != null && transform.tag == "Sword"){
+    //     WalkToTarget.Knockback(transform);
+    // }
 }
 }
