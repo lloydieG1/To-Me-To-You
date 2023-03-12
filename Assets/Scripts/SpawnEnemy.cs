cc718330f23303d0b74ed73ebdf140129e2d7991
lloydieG1 <lloydieG1@gmail.com>
2023-03-12 15:46:36 +0000

    Walker is now a prefab

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 2f;
+    public float spawnRadius = 10f;
+
+    private float timeSinceLastSpawn;
+
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= spawnInterval)
+        {
+            SpawnEnemy();
+            timeSinceLastSpawn = 0f;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 randomSpawnPoint = transform.position + Random.insideUnitSphere * spawnRadius;
+        Instantiate(enemyPrefab, randomSpawnPoint, Quaternion.identity);
+    }
+}
+
