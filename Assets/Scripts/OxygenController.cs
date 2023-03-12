9be2ec4f4150023f5fe54b2f44ab8026e91377c1
lloydieG1 <lloydieG1@gmail.com>
2023-03-12 02:14:18 +0000

    oxygen meter is renewed with faces!

--- Assets/Scripts/OxygenController.cs
+++ Assets/Scripts/OxygenController.cs
@@ -10,44 +10,63 @@ public class OxygenController : MonoBehaviour
     public float oxygenGainRate;    // rate at which oxygen refills per second
     public float colorChangeSpeed;
 
-    public Image oxygenBar;
+    public GameObject oxygenUI;
+    private OxygenBar oxygenBar;
+
     private float width;
     private float initialOxygenLevel;
 
     Color currentColor;
     Color targetColor;
     Color newColor;
-    float percentageDepleted;
+    int roundedPercentageDepleted;
 
     private void Start() {
         initialOxygenLevel = oxygenLevel;
+
+        oxygenBar = oxygenUI.GetComponent<OxygenBar>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // currentColor = oxygenBar.color;
-        // targetColor = oxygenGradient.Evaluate(oxygenLevel / 100f);
-        // newColor = Color.Lerp(currentColor, targetColor, Time.deltaTime * colorChangeSpeed);
+        roundedPercentageDepleted = Mathf.RoundToInt(oxygenLevel/initialOxygenLevel * 100); 
 
-        // oxygenBar.color = newColor;
-        percentageDepleted = oxygenLevel/initialOxygenLevel; 
-        
-        if(percentageDepleted < 0.5 && percentageDepleted > 0.25) {
-            oxygenBar.color = new Color(255,255,0);
-        } else if(oxygenLevel/initialOxygenLevel < 0.25) {
-            oxygenBar.color = new Color(255,0,0);
+        switch (roundedPercentageDepleted)
+        {
+            case 100:
+                oxygenBar.ChangeSprite(0);
+                Debug.Log("100% remaining");
+                break;
+            case 75:
+                oxygenBar.ChangeSprite(1);
+                Debug.Log("75% remaining");
+                break;
+            case 50:
+                oxygenBar.ChangeSprite(2);
+                Debug.Log("50% remaining");
+                break;
+            case 25:
+                oxygenBar.ChangeSprite(3);
+                Debug.Log("20% remaining");
+                break;
+            case 10:
+                oxygenBar.ChangeSprite(4);
+                Debug.Log("10% remaining");
+                break;
+            case 0:
+                oxygenBar.ChangeSprite(5);
+                Debug.Log("5% remaining");
+                break;
+            default:
+                Debug.Log("invalid case number");
+                break;
         }
         
         // Deplete oxygen based on depletion rate
         oxygenLevel -= oxygenDepletionRate * Time.deltaTime;
 
-
-        // update oxygen display
-        width = oxygenLevel;
-
-        oxygenBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
 
         // Clamp oxygen level to be between 0 and 100
         oxygenLevel = Mathf.Clamp(oxygenLevel, 0f, 100f);
